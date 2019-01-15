044b6b75a9d93c2459a1ba019f6d7dd9e9afce79
laboratorio laboratorio <laboratorio@labinfo.is.escuelaing.edu.co>
2019-01-15 10:21:21 -0500

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+public class Main {
+    public static void main (String [ ] args) {
+        LinkedList<Integer> lista = new LinkedList<Integer>();
+        lista.add(123);
+        lista.add(156);
+        System.out.println("vfdfvfd");
+
+
+    }
+}
